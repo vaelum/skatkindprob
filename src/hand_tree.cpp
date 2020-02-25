030669e7fb1ff068eeba6201e624f489c37fb4b7
amos <amos@vaelum.dev>
2020-02-25 09:46:40 +0100

    add main datastructure calculating hands
    GitOrigin-RevId: a2c7a479a5ecb36f3188dd9303253e51bb89fa4c

---
+++ src/hand_tree.cpp
@@ -0,0 +1,86 @@
+#include "hand_tree.h"
+
+HandTree::HandTree() {
+  Hand hand;
+  mainLeaf = std::make_shared<Leaf>(nullptr, 0, hand);
+
+  this->populate(mainLeaf, hand, 0);
+}
+
+HandTree::~HandTree() {}
+
+void HandTree::populate(std::shared_ptr<Leaf> leaf, Hand hand,
+                        unsigned char depth) {
+  // remove after verifying no longer needed
+  if (depth > 7) {
+    std::cout << "SOMETHING WENT WRONG!!! No. 1" << std::endl;
+    return;
+  }
+
+  for (unsigned char i = 0; i < 5; i++) {
+    hand.symbol[depth] = i;
+    if (this->isValidHand(hand)) {
+      if (depth + 1 == 8) {
+        unsigned char sum = 0;
+        for (auto v : hand.symbol) {
+          sum += v;
+        }
+        if (sum == 10) {
+          leaf->children[i] = std::make_shared<Leaf>(leaf, depth + 1, hand);
+        }
+      } else {
+        leaf->children[i] = std::make_shared<Leaf>(leaf, depth + 1, hand);
+        this->populate(leaf->children[i], hand, depth + 1);
+      }
+    } else {
+      leaf->children[i] = nullptr;
+    }
+  }
+}
+
+bool HandTree::isValidHand(Hand &hand) {
+  unsigned char sum = 0;
+  for (auto v : hand.symbol) {
+    sum += v;
+  }
+  return sum <= 10 ? true : false;
+}
+
+std::vector<Hand> HandTree::getAllHands(std::vector<Hand> &dealedHands) {
+  std::vector<Hand> retHands;
+  this->getAllHandsRec(mainLeaf, dealedHands, retHands);
+  return retHands;
+}
+
+void HandTree::getAllHandsRec(std::shared_ptr<Leaf> leaf,
+                              std::vector<Hand> &dealedHands,
+                              std::vector<Hand> &retHands) {
+
+  std::vector<Hand> checkHands(dealedHands);
+  checkHands.push_back(leaf->hand);
+
+  if (this->areCompatibleHands(checkHands)) {
+    if (leaf->depth == 8) {
+      retHands.push_back(leaf->hand);
+      return;
+    }
+    for (unsigned char i = 0; i < 5; i++) {
+      if (leaf->children[i] != nullptr) {
+        this->getAllHandsRec(leaf->children[i], dealedHands, retHands);
+      }
+    }
+  }
+}
+
+bool HandTree::areCompatibleHands(std::vector<Hand> &hands) {
+  for (unsigned char i = 0; i < 8; i++) {
+    unsigned char sum = 0;
+    for (auto h : hands) {
+      sum += h.symbol[i];
+    }
+    if (sum > 4) {
+      return false;
+    }
+  }
+  return true;
+}
---
+++ src/hand_tree.h
@@ -0,0 +1,54 @@
+#ifndef HAND_TREE_H
+#define HAND_TREE_H
+#include <array>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <string>
+#include <vector>
+
+struct Hand {
+  unsigned char symbol[8];
+  Hand() {
+    symbol[0] = 0;
+    symbol[1] = 0;
+    symbol[2] = 0;
+    symbol[3] = 0;
+    symbol[4] = 0;
+    symbol[5] = 0;
+    symbol[6] = 0;
+    symbol[7] = 0;
+  }
+};
+
+struct Leaf {
+  std::array<std::shared_ptr<Leaf>, 5> children;
+  std::shared_ptr<Leaf> parent;
+  unsigned char depth;
+  Hand hand;
+  Leaf(std::shared_ptr<Leaf> parent, unsigned char depth, Hand hand) {
+    this->parent = parent;
+    this->depth = depth;
+    this->hand = hand;
+  }
+};
+
+class HandTree {
+public:
+  HandTree();
+  ~HandTree();
+
+  std::vector<Hand> getAllHands(std::vector<Hand> &dealedHands);
+
+private:
+  void populate(std::shared_ptr<Leaf> leaf, Hand hand, unsigned char depth);
+  bool isValidHand(Hand &hand);
+  bool areCompatibleHands(std::vector<Hand> &hands);
+  void getAllHandsRec(std::shared_ptr<Leaf> leaf,
+                      std::vector<Hand> &dealedHands,
+                      std::vector<Hand> &retHands);
+
+  std::shared_ptr<Leaf> mainLeaf;
+};
+
+#endif // HAND_TREE_H
