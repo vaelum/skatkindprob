d24d9ffa65ba07f519bd8e1ae8ae345b11ce306a
amos <amos@vaelum.dev>
2020-02-27 12:44:24 +0100

    add namespace io with progressbar()
    GitOrigin-RevId: 9e45ba818450e7e6370fa291950ccd4dec7cccf7

---
+++ src/io.cpp
@@ -0,0 +1,18 @@
+#include "io.h"
+
+void io::progressbar(float progress) {
+  int barWidth = 70;
+
+  std::cout << "[";
+  int pos = barWidth * progress;
+  for (int i = 0; i < barWidth; ++i) {
+    if (i < pos)
+      std::cout << "=";
+    else if (i == pos)
+      std::cout << ">";
+    else
+      std::cout << " ";
+  }
+  std::cout << "] " << int(progress * 100.0) << " %\r";
+  std::cout.flush();
+}
---
+++ src/io.h
@@ -0,0 +1,10 @@
+#ifndef IO_NS
+#define IO_NS
+#include <iostream>
+namespace io {
+
+void progressbar(float progress);
+
+} // namespace io
+
+#endif // IO_NS
